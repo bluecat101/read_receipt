66a8679f10951ae0ba8f3db544a73812d9f8816c
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2023-06-04 16:22:42 +0900

    ADD function to write for file

--- gui.py
+++ gui.py
@@ -1,3 +1,5 @@
+import copy as cp
+import csv
 import tkinter as tk
 from  tkinter import ttk
 import item_db as db
@@ -44,7 +46,8 @@ class ComfirmReciept(tk.Frame):
     for i,column in enumerate(columns): 
       columnLabel=tk.Label(self.tableFrame,text=column,width=7,padx=3,anchor="center",bg="#3A3A3A")
       columnLabel.grid(row=0,column=i)
-    ttk.Label(self.tableFrame).grid(row=0,column=7)
+    tk.Frame(self.tableFrame).grid(row=0,column=7)
+    # columnLabel.grid(row=0,column=7)
     
     self.itemList=[]
     for primary_item_key,primary_item_value in db.itemDB.items():
@@ -54,39 +57,39 @@ class ComfirmReciept(tk.Frame):
 
     for i,item in enumerate(items):
       hasDiscount= len(item)>5
-      productNameLabel=tk.Entry(self.tableFrame, width=7,justify="center",bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
-      productNameLabel.insert(0,item[0])
-      productNameLabel.grid(row=i+1,column=0)
+      productNameEntry=tk.Entry(self.tableFrame, width=7,justify="center",bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
+      productNameEntry.insert(0,item[0])
+      productNameEntry.grid(row=i+1,column=0)
 
-      itemLabel=ttk.Combobox(self.tableFrame,value=self.itemList,width=7)
-      itemLabel.set(item[1])
-      itemLabel.grid(row=i+1,column=1)
+      itemEntry=ttk.Combobox(self.tableFrame,value=self.itemList,width=7)
+      itemEntry.set(item[1])
+      itemEntry.grid(row=i+1,column=1)
 
-      priceLabel=tk.Entry(self.tableFrame,width=7,justify="center",bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
-      priceLabel.insert(0,item[2])
-      priceLabel.grid(row=i+1,column=2)
+      priceEntry=tk.Entry(self.tableFrame,width=7,justify="center",bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
+      priceEntry.insert(0,item[2])
+      priceEntry.grid(row=i+1,column=2)
 
-      amountLabel=tk.Entry(self.tableFrame,width=7,justify="center",bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
-      amountLabel.insert(0,item[3])
-      amountLabel.grid(row=i+1,column=3)
+      amountEntry=tk.Entry(self.tableFrame,width=7,justify="center",bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
+      amountEntry.insert(0,item[3])
+      amountEntry.grid(row=i+1,column=3)
 
-      discountLabel=tk.Entry(self.tableFrame,width=7,justify="center",bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
-      discountLabel.insert(0,item[4])
-      discountLabel.grid(row=i+1,column=4)
+      discountEntry=tk.Entry(self.tableFrame,width=7,justify="center",bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
+      discountEntry.insert(0,item[4])
+      discountEntry.grid(row=i+1,column=4)
 
-      totalLabel=tk.Entry(self.tableFrame,width=7,justify="center",bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
+      totalEntry=tk.Entry(self.tableFrame,width=7,justify="center",bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
       if hasDiscount:
-        totalLabel.insert(0,item[5])
+        totalEntry.insert(0,item[5])
       else:
-        totalLabel.insert(0,"---")
-      totalLabel.grid(row=i+1,column=5)
+        totalEntry.insert(0,"---")
+      totalEntry.grid(row=i+1,column=5)
 
-      totalLabel=tk.Entry(self.tableFrame,width=7,justify="center",bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
+      totalEntry=tk.Entry(self.tableFrame,width=7,justify="center",bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
       if hasDiscount:
-        totalLabel.insert(0,int(item[3])*int(item[4])-int(item[5]))
+        totalEntry.insert(0,int(item[3])*int(item[4])-int(item[5]))
       else:
-        totalLabel.insert(0,int(item[3])*int(item[4]))
-      totalLabel.grid(row=i+1,column=6)
+        totalEntry.insert(0,int(item[3])*int(item[4]))
+      totalEntry.grid(row=i+1,column=6)
       
       deleteButton=ttk.Button(self.tableFrame,text="delete")
       deleteButton.bind("<ButtonPress>",self.deleteItem)
@@ -109,26 +112,26 @@ class ComfirmReciept(tk.Frame):
   def addItem(self):
     row=int(len(self.tableFrame.winfo_children())/8)-1
 
-    productNameLabel=tk.Entry(self.tableFrame,width=7,bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
-    productNameLabel.grid(row=row+1,column=0)
+    productNameEntry=tk.Entry(self.tableFrame,width=7,bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
+    productNameEntry.grid(row=row+1,column=0)
 
-    itemLabel=ttk.Combobox(self.tableFrame,value=self.itemList,width=7)
-    itemLabel.grid(row=row+1,column=1)
+    itemEntry=ttk.Combobox(self.tableFrame,value=self.itemList,width=7)
+    itemEntry.grid(row=row+1,column=1)
 
-    priceLabel=tk.Entry(self.tableFrame,width=7,bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
-    priceLabel.grid(row=row+1,column=2)
+    priceEntry=tk.Entry(self.tableFrame,width=7,bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
+    priceEntry.grid(row=row+1,column=2)
 
-    amountLabel=tk.Entry(self.tableFrame,width=7,bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
-    amountLabel.grid(row=row+1,column=3)
+    amountEntry=tk.Entry(self.tableFrame,width=7,bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
+    amountEntry.grid(row=row+1,column=3)
 
-    discountLabel=tk.Entry(self.tableFrame,width=7,bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
-    discountLabel.grid(row=row+1,column=4)
+    discountEntry=tk.Entry(self.tableFrame,width=7,bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
+    discountEntry.grid(row=row+1,column=4)
 
-    totalLabel=tk.Entry(self.tableFrame,width=7,bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
-    totalLabel.grid(row=row+1,column=5)
+    totalEntry=tk.Entry(self.tableFrame,width=7,bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
+    totalEntry.grid(row=row+1,column=5)
 
-    totalLabel=tk.Entry(self.tableFrame,width=7,bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
-    totalLabel.grid(row=row+1,column=6)
+    totalEntry=tk.Entry(self.tableFrame,width=7,bg="#4B4B4B",borderwidth=-0.5,highlightbackground="#565656",relief="flat")
+    totalEntry.grid(row=row+1,column=6)
 
     
     deleteButton=ttk.Button(self.tableFrame,text="delete")
@@ -146,22 +149,36 @@ class ComfirmReciept(tk.Frame):
         else:
           element.configure(foreground="white")
       elif type(element) == tk.Entry:
-        if ((i%8 >= 3 and not(element.get().isdecimal())) and (i%8 != 5)) or (("---" in element.get()) and (i%8 != 5)):
+        if i%8 < 3 and (("---" in element.get()) or element.get()==""):
+          isOk=False
+          element.configure(highlightbackground="red")
+        elif i%8 >= 3 and not(element.get().isdecimal()) and i%8 != 5:
           isOk=False
           element.configure(highlightbackground="red")
         else:
           element.configure(highlightbackground="#565656")
     if isOk==True:
-      print("submit")
-      # for element in elements:
-        # print(element.get())
-
+      path = 'output.csv'
+      f = open(path, mode='a')
+      oneLine=[]
+      allItem=[]
+      for i,element in enumerate(elements):
+        if type(element) == tk.Entry or type(element) == ttk.Combobox:
+          oneLine.append(element.get())
+          if i%8==6:
+            allItem.append(cp.copy(oneLine))
+            oneLine.clear()
+      writer = csv.writer(f)
+      for lineItem in allItem :
+        writer.writerow(lineItem)
+      f.close()
+      print("write for file")
 # self.root = tk.Tk()
-# if __name__=="__main__":
-# #   self.root = tk.Tk()
-#   items=[["fff",2,3,4,5,6],[1+1,2+1,3+1,4,6+1],["fff",2,3,4,5,6],[1+1,2+1,3+1,4,6+1],["fff",2,3,4,5,6],[1+1,2+1,3+1,4,6+1],["fff",2,3,4,5,6],[1+1,2+1,3+1,4,6+1]]
-#   app=ComfirmReciept(items)
-#   app.mainloop()
+if __name__=="__main__":
+#   self.root = tk.Tk()
+  items=[["fff",2,3,4,5,6],[1+1,2+1,3+1,4,6+1],["fff",2,3,4,5,6],[1+1,2+1,3+1,4,6+1],["fff",2,3,4,5,6],[1+1,2+1,3+1,4,6+1],["fff",2,3,4,5,6],[1+1,2+1,3+1,4,6+1]]
+  app=ComfirmReciept(items)
+  app.mainloop()
     
 
 
