9666981ea7134e655f97a36c20e1d5d8947cd303
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2023-06-06 17:41:57 +0900

    refactoring to change DB specification

--- record_receipt.py
+++ record_receipt.py
@@ -7,18 +7,11 @@ ITEMDB_PATH_NAME = "item_db.py"
 
 class RecordReceipt:
   itemDB = db.itemDB
+  categoryDB = db.categoryDB
   def __init__(self,allItem,newCategoryEn):
     self.allItem=allItem
-    # self.writeFile()
     self.newCategoryEn=newCategoryEn
-    # print(self.newCategoryEn)
-
     self.addDB()
-    # if  newCategoryEn=={}:
-    #   print(newCategoryEn)
-    #   print(type(newCategoryEn))
-    
-    # if 
 
   def writeFile(self):
     f = open(CSV_PATH_NAME, mode='a')
@@ -31,69 +24,40 @@ class RecordReceipt:
 
 
   def addDB(self):
-    originCategoryNum=len(self.itemDB.keys())
-    # print(originCategoryNum)
     newCategory={}
-    # newCategoryEn={}
     for lineItem in self.allItem:
       if not(lineItem[2] in self.itemDB):
         self.itemDB[lineItem[2]] = []# 追加する
         self.itemDB[lineItem[2]].append(lineItem[1])
 
         newCategory[lineItem[2]] = []# 追加する
-        # newCategory[lineItem[2]].append(lineItem[1])
       elif not(lineItem[1] in self.itemDB[lineItem[2]]):
         self.itemDB[lineItem[2]].append(lineItem[1])
+
     for key in newCategory:
       newCategory[key]=self.itemDB[key]
-
-    fRead = open(ITEMDB_PATH_NAME,"r")
-    lineFILE = fRead.readlines()
-    currentLine=0
     replaceContent=""
-    categoryList=self.itemDB.copy()
+
     for key,val in self.itemDB.items():
-      try:
-        categoryName = re.match('^.+=',lineFILE[currentLine]).group()
-        categoryName=categoryName[:-2]
-        # print(repr(categoryName))
-      except:
-        categoryName = "noname"
-        # replaceContent+=lineFILE[currentLine]
-      categoryList[key]=categoryName
-      if not(str(val) in lineFILE[currentLine]):
-        replaceContent+=categoryName+" = "+str(val)+'\n'
+      if key in newCategory:
+        replaceContent += newCategoryEn[key] +" = "+str(val)+'\n'
       else:
-        replaceContent+=lineFILE[currentLine]
-      currentLine+=1
-      if(currentLine >= originCategoryNum):
-        break
+        replaceContent += self.categoryDB[key] + " = " +str(val)+'\n'
 
-    for key in newCategory:
-      categoryList[key]=self.newCategoryEn[key]
-
-    # print(newCategory)
-    # print(categoryList)
-
-    for key,value in newCategory.items():
-      # print(key)
-      replaceContent += self.newCategoryEn[key] +" = "+ str(value) + '\n'
-    # print(self.itemDB)
-    # print(categoryList)
-
+    for key,value in newCategoryEn.items():
+      self.categoryDB[key]=value 
+    
     replaceContent+= "itemDB = {"
-    for key in self.itemDB.keys():
-      replaceContent += "'" + key + "': " + categoryList[key] +", "
+    for key in self.categoryDB:
+      replaceContent += "'" + key + "': " + self.categoryDB[key] +", "
     replaceContent=replaceContent[:-2]
     replaceContent += '}\n'
-    # print(replaceContent)
-    fRead.close()
+    replaceContent += "categoryDB = " + str(self.categoryDB) + '\n'
     with open(ITEMDB_PATH_NAME,"w") as f:
       f.write(replaceContent)
-    # print("------")
-    # print(self.itemDB)
 
 
+""""""
 if __name__=="__main__":
   items=[
         ['2', '牛乳', '乳製品', '4', '7', '0', '28']
@@ -102,5 +66,6 @@ if __name__=="__main__":
         ,['fff', 'newItem', '新しい', '4', '5', '6', '14']
         ,['fff', 'newItem2', '新しい', '4', '5', '6', '14']
         ]
-  newCategoryEn={"新しい": "newcategory"}
+  # newCategoryEn={"新しい": "newcategory"}
+  newCategoryEn={}
   app=RecordReceipt(items,newCategoryEn)
