9f277285c76b776f59a90e5115ef8660b3259985
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-02-06 12:32:33 +0900

    make special discount

--- main.py
+++ main.py
@@ -5,14 +5,29 @@ import gui as ui
 import record_receipt as recordre
 from tkinter import filedialog
 
+TEST = True
 
 def main(filename):
-  receiptInfo=readre.ReadReceipt(filename)
-  Itemline=receiptInfo.getItemLine()
-  store=receiptInfo.getStore()
-  date=receiptInfo.getDate()
-  
-  gui=ui.ComfirmReciept(Itemline,date,store)
+  if TEST:
+    Itemline=[
+    ['深蒸し緑茶', '緑茶', '飲み物', 498, '2'],
+    # ['クーポン嗜好品20%', 'bb', 'aa', '200', 1],
+    # ['揚げせん', 'item', 'cc', 90, '2'],
+    # ['深蒸し緑茶', '緑茶', 'dd', 498, '2'],
+    # ['クーポン嗜好品20%', 'ee', 'aa', '200', 1],
+    # ['揚げせん', 'item', 'ff', 90, '2']
+    ]
+    date="2023/4/9"
+    store="ダイエーaaa"
+    specialDiscount =[["フルグラ",50],["割引",532]]
+  else:
+    receiptInfo=readre.ReadReceipt(filename)
+    Itemline=receiptInfo.getItemLine()
+    store=receiptInfo.getStore()
+    date=receiptInfo.getDate()
+    specialDiscount = receiptInfo.specialDiscount
+  # print(Itemline)
+  gui=ui.ComfirmReciept(Itemline,date,store,specialDiscount)
   gui.mainloop()
   
   if not(gui.getHasIssue()):
@@ -20,23 +35,16 @@ def main(filename):
   
 
 
-Itemline=[
-['深蒸し緑茶', '緑茶', '飲み物', 498, '2'],
-# ['クーポン嗜好品20%', 'bb', 'aa', '200', 1],
-# ['揚げせん', 'item', 'cc', 90, '2'],
-# ['深蒸し緑茶', '緑茶', 'dd', 498, '2'],
-# ['クーポン嗜好品20%', 'ee', 'aa', '200', 1],
-# ['揚げせん', 'item', 'ff', 90, '2']
-]
-date="2023/4/9"
-store="ダイエーaaa"
+
 
 if __name__ == "__main__":
-  filename = filedialog.askopenfilename(
-    title = "レシートを選択してください。",
-    filetypes = [("Image file", ".png .jpg "),("PNG", ".png"), ("JPEG", ".jpg")], # ファイルフィルタ
-    initialdir = "./" # 自分自身のディレクトリ
+  if TEST:
+    main("test")
+  else:
+    filename = filedialog.askopenfilename(
+      title = "レシートを選択してください。",
+      filetypes = [("Image file", ".png .jpg "),("PNG", ".png"), ("JPEG", ".jpg")], # ファイルフィルタ
+      initialdir = "./" # 自分自身のディレクトリ
     )
-  if filename != "":
-    main(filename)
-
+    if filename != "":
+      main(filename)
