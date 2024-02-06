4b30e5277bc3ed963d1271ff557a230a353ffba5
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-02-06 12:27:32 +0900

    ADD special discount

--- gui.py
+++ gui.py
@@ -8,7 +8,7 @@ import re
 hasIssue = True # Whether System behavior is nomal
 class ComfirmReciept(tk.Frame):
   root = tk.Tk() # make display
-  def __init__(self,items,date,store): 
+  def __init__(self,items,date,store,discount): 
     super().__init__(self.root)
     
     self.root.title("comfirm detail") # title 
@@ -16,6 +16,8 @@ class ComfirmReciept(tk.Frame):
     self.root.update_idletasks()      # updadate root size (default is 1x1)
     self.date=date   # to instance variable
     self.store=store # to instance variable
+    self.discount = discount
+    self.discount_total = sum(list(zip(*discount))[1])
 
     storeDateFrame=tk.Frame(self.root,height=27) # Frame for store name and date 
     storeDateFrame.pack(pady=0)                  # set position
@@ -124,14 +126,43 @@ class ComfirmReciept(tk.Frame):
     parentFrame.update_idletasks()                                     # update parentFrame size because tableFrame is added many item and parentFrame include tableFrame
     functionFrame=tk.Frame(self.root)                                  # include decide , back ,next button and total label
     functionFrame.propagate(False)                                     # can not chage automatic
-    functionFrame.configure(width=parentFrame.winfo_width(),height=30) # set Style
+    functionFrame.configure(width=parentFrame.winfo_width(),height=200) # set Style
     functionFrame.pack()                                               # set position
 
     
-    totalLabel=tk.Label(functionFrame,text="合計")      # Label for "TOTAL"
-    totalLabel.place(relx=0.8)                         # set position
-    self.totalNumLabel=tk.Label(functionFrame,text=0)  # Label for total 
-    self.totalNumLabel.place(relx=0.9)                 # set position
+    result_tk ={"frame":tk.Frame(functionFrame,relief=tk.SOLID)}
+    result_tk["frame"].configure(width=parentFrame.winfo_width()/4,height=200) # set Style
+    print(parentFrame.winfo_width())
+    print(result_tk["frame"].winfo_width(),result_tk["frame"].winfo_height())
+    print(functionFrame.winfo_width(),functionFrame.winfo_height())
+    result_tk["frame"].place(relx=0.75)
+    
+    result_tk.update(
+      subtotal = tk.Label(result_tk["frame"],text="小計"),
+      discount = tk.Label(result_tk["frame"],text="割引"),
+      tax      = tk.Label(result_tk["frame"],text="税"),
+      total    = tk.Label(result_tk["frame"],text="合計"),
+    )
+    self.result_tk_value = {
+      "subtotal" :tk.Label(result_tk["frame"],text=0),
+      "discount" :tk.Entry(result_tk["frame"],width=5, justify=tk.RIGHT,bg="#4B4B4B"),
+      "tax"      :tk.Label(result_tk["frame"],text=0),
+      "total"    :tk.Label(result_tk["frame"],text=0)
+    }
+    self.result_tk_value["discount"].insert(0,self.discount_total)
+    self.result_tk_value["discount"].bind("<Enter>", self.display_discount_detail)
+    self.result_tk_value["discount"].bind("<Leave>", self.leave_discount_detail)
+    self.result_tk_value["discount"].bind("<Return>",self.calculateEvent)
+    for i,key_value in enumerate(result_tk.items()):
+      key ,value = key_value
+      if key == "frame":
+        continue
+      else:
+        value.grid(row = i-1, column = 0)
+        self.result_tk_value[key].grid(row = i-1, column = 1,sticky=tk.E)
+        unit_label = tk.Label(result_tk["frame"],text ="円")
+        unit_label.grid(row = i-1, column = 2)
+        
 
     decideButton=ttk.Button(functionFrame,text="決定",command=self.decideItem) # Button for decide function
     decideButton.place(relx=0.35)                                             # set position
@@ -142,13 +173,48 @@ class ComfirmReciept(tk.Frame):
     
     self.updateRegion() # update field
     self.calculate()    # calcukate total
-    
+    # print(functionFrame.winfo_width(),functionFrame.winfo_height())
+    # print(resultFrame.winfo_width(),resultFrame.winfo_height())
+
     storeDateFrame.configure(width=parentFrame.winfo_width()) # update parentFrame width
 
 
     self.root.bind_class("Entry", "<FocusOut>", self.calculateEvent) # Add Event for Entry
 
+  def display_discount_detail(self,event):
+    x = self.result_tk_value["discount"].winfo_rootx()+ 25
+    y = self.result_tk_value["discount"].winfo_rooty()+ 20
+    h = len(self.discount)*50
+    w = 50
+    self.discount_detail = tk.Toplevel(self.result_tk_value["discount"])
+    self.discount_detail.wm_overrideredirect(True)
+    self.discount_detail.wm_geometry("%dx%d+%d+%d" % (h,w,x, y))
+    
+    for i,d in enumerate(self.discount):
+      name_label = tk.Label(self.discount_detail,text=d[0])
+      value_label = tk.Label(self.discount_detail,text=d[1])
+      name_label.grid(row = i, column = 0)
+      value_label.grid(row = i, column = 1)
+    return
+  def leave_discount_detail(self,event): 
+    tw = self.discount_detail
+    self.discount_detail= None
+    if tw:
+      tw.destroy()
+    return
+  
+  # def caculate_result(self,event):
+  #   self.result_tk_value["subtotal"].configure(text=subtotal)                        # insert subtotal num and "yesn"
+  #   discount = int(self.result_tk_value["discount"].get())
+  #   tax = int((subtotal-discount)*0.08)
+  #   self.result_tk_value["tax"].configure(text = tax)                        # insert tax num and "yesn"
+  #   self.result_tk_value["total"].configure(text = subtotal - discount + tax)                        # insert total num and "yesn"
+  # def calculate(self,event): # execute when tk.Entry FocusOut
+  #   print("aaa")
+  #   self.calculate()              # call calculate function
+
   def categoryEvent(self,event): # When mouse enter categoryCombobox
+    subtotal = 0     # total 
     elements=self.tableFrame.winfo_children()  # get all item in tableFrame
     if event.widget.get() in db.itemDB.keys(): # category name include in register category name
       elements[elements.index(event.widget)-1].configure(value=[value for value in db.itemDB[event.widget.get()]]) # change text of register item name 
@@ -161,7 +227,7 @@ class ComfirmReciept(tk.Frame):
   def calculate(self): # calucute
     elements = self.tableFrame.winfo_children() # get all item in tableFrame
     eachTotal = 0 # each item 
-    total = 0     # total 
+    subtotal = 0     # total 
     isInt = True  # item price and item amount are int type 
     for i,element in enumerate(elements):  
       if i%8 == 3 or i%8 == 4:                                                           # for item price or item amount
@@ -180,10 +246,14 @@ class ComfirmReciept(tk.Frame):
         if isInt:                                                                        # integer or not
           element.delete(0,tk.END)                                                       # delete text in the total Label
           element.insert(0,eachTotal)                                                    # input total
-          total+=eachTotal                                                               # add each item total to total
+          subtotal+=eachTotal                                                               # add each item total to total
         eachTotal=0
         isInt=True                                                                       # init variable
-    self.totalNumLabel.configure(text=str(total)+"円")                                    # insert total num and "yesn"
+    self.result_tk_value["subtotal"].configure(text=subtotal)                        # insert subtotal num and "yesn"
+    discount = int(self.result_tk_value["discount"].get())
+    tax = int((subtotal-discount)*0.08)
+    self.result_tk_value["tax"].configure(text = tax)                        # insert tax num and "yesn"
+    self.result_tk_value["total"].configure(text = subtotal - discount + tax)                        # insert total num and "yesn"
   
 
   def updateRegion(self):
