b2e2f6533f85c148ed0178a33f7e3b3e5f4ad670
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-02-07 16:34:25 +0900

    Update data

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ import record_receipt as recordre
 from tkinter import filedialog
 
 TEST = False
+TEST2 = True
 
 def main(filename):
   if TEST:
+    Itemline=receiptInfo.getItemLine()
+    store=receiptInfo.getStore()
+    date=receiptInfo.getDate()
     Itemline=[
     ['深蒸し緑茶', '緑茶', '飲み物', 498, '2'],
     # ['クーポン嗜好品20%', 'bb', 'aa', '200', 1],
@@ -18,8 +22,15 @@ def main(filename):
     # ['揚げせん', 'item', 'ff', 90, '2']
     ]
     date="2023/4/9"
-    store="ダイエーaaa"
-    specialDiscount =[["フルグラ",50],["割引",532]]
+    store="ダイエー"
+    # specialDiscount =[["フルグラ",50],["割引",532]]
+    specialDiscount = []
+  elif TEST2:
+    receiptInfo=readre.ReadReceipt(filename,TEST2)
+    Itemline=receiptInfo.getItemLine()
+    store=receiptInfo.getStore()
+    date=receiptInfo.getDate()
+    specialDiscount = receiptInfo.specialDiscount
   else:
     receiptInfo=readre.ReadReceipt(filename)
     Itemline=receiptInfo.getItemLine()
@@ -31,7 +42,7 @@ def main(filename):
   gui.mainloop()
   
   # if gui.isOk:
-  recordOb=recordre.RecordReceipt(gui.getAllItem(),gui.newCategory,gui.date,gui.store)
+  recordOb=recordre.RecordReceipt(gui.getAllItem(),gui.newCategory,gui.date,gui.store[0])
   
 
 
@@ -39,6 +50,8 @@ def main(filename):
 
 if __name__ == "__main__":
   if TEST:
+    main("test")
+  elif TEST2:
     main("test")
   else:
     filename = filedialog.askopenfilename(
--- store_db.py
+++ store_db.py
@@ -1,1 +1,28 @@
-storeDB = ['ダイエー', 'ローゼン', 'イオン', 'ダイエーaaa']
+storeDB = ['ダイエー', 'ローゼン', 'イオン', 'FamilyMart',"セブン-イレブン"]
+target ={'ダイエー':"", 'ローゼン':"", 'イオン':"", 'FamilyMart':"領収証","セブン-イレブン":"領収書"}
+
+def setting(store_name,text):
+  if store_name == "":
+    return text
+  elif store_name == "ダイエー":
+    return text
+  elif store_name == "ローゼン":
+    return text
+  elif store_name == "イオン":
+    return text
+  elif store_name == "FamilyMart":
+    if len(text) >0:
+      idx = text.find("¥")
+      if idx != -1:
+        return text[:idx] + text[idx+1:]
+    return text
+  else:
+    return text
+
+def tax(store_name):
+  include_tax = ["FamilyMart"]
+  exclude_tax = ["","ダイエー","ローゼン","イオン","セブン-イレブン"]
+  if store_name in include_tax:
+    return "内税"
+  else:
+    return "外税"
