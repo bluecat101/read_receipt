1b804c4d65d0b110133cc55066271c852171738a
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-02-16 15:59:03 +0900

    Adapt for 業務スーパー

--- store_db.py
+++ store_db.py
@@ -1,3 +1,4 @@
+import re
 storeDB = ['ダイエー', 'ローゼン', 'イオン', 'FamilyMart', 'セブン-イレブン', '吉野家', 'まかいの牧場', 'EXCELSIOR CAFFE', '業務スーパー', 'KAMUKURA', 'LAWSON', 'EXCELSIOR CAFFEEXCELSIOR CAFFE', '業務スーパー', '山安', '文左亭', 'LAWSON', 'KALDI', '世界一のアップルパイ', 'サービスエリア', 'NewDays', 'STARBUCKS', 'アトレ', 'クリエイト']
 target = {'ダイエー': '', 'ローゼン': '', 'イオン': '', 'FamilyMart': '領収証', 'セブン-イレブン': '領収書', '吉野家': '', 'まかいの牧場': '', '業務スーパー': '', 'EXCELSIOR CAFFE': '', 'KAMUKURA': '', 'LAWSON': '', 'EXCELSIOR CAFFEEXCELSIOR CAFFE': '', '山安': '', '文左亭': '', 'KALDI': '', '世界一のアップルパイ': '', 'サービスエリア': '', 'NewDays': '', 'STARBUCKS': '', 'アトレ': '', 'クリエイト': ''}
 
@@ -7,6 +8,12 @@ def setting(store_name,text):
   elif store_name == "ダイエー":
     return text
   elif store_name == "ローゼン":
+    if len(text) >0:
+      extra_char = re.match("\*?{[0-9]|[A-Z]}+",text)
+      text = text[extra_char.end():]
+      idx = text.find("¥")
+      if idx != -1:
+        return text[:idx] + text[idx+1:]
     return text
   elif store_name == "イオン":
     return text
@@ -19,6 +26,14 @@ def setting(store_name,text):
   elif store_name == "セブン-イレブン":
     if len(text) >0:
       idx = text.find("*")
+      if idx != -1:
+        return text[:idx] + text[idx+1:]
+    return text
+  elif store_name == "業務スーパー":
+    if len(text) >0:
+      extra_char = re.match("{[0-9]|[A-Z]}+",text)
+      text = text[extra_char.end():]
+      idx = text.find("¥")
       if idx != -1:
         return text[:idx] + text[idx+1:]
     return text
