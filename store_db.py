ec1ec75ae941f879fe50701eeb4c6ad5a6e0941d
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-02-28 20:21:02 +0900

    Update data

--- store_db.py
+++ store_db.py
@@ -1,5 +1,6 @@
-storeDB = ['ダイエー', 'ローゼン', 'イオン', 'FamilyMart', 'セブン-イレブン', '吉野家', 'まかいの牧場', 'EXCELSIOR CAFFE', '業務スーパー', 'KAMUKURA', 'LAWSON', 'EXCELSIOR CAFFEEXCELSIOR CAFFE', '業務スーパー', '山安', '文左亭', 'LAWSON', 'KALDI', '世界一のアップルパイ', 'サービスエリア', 'NewDays', 'STARBUCKS', 'アトレ', 'クリエイト', ' くら寿司', 'お菓子のゆりかご', 'ダイソー', 'くら寿司']
-target = {'ダイエー': '', 'ローゼン': '', 'イオン': '', 'FamilyMart': '領収証', 'セブン-イレブン': '領収書', '吉野家': '', 'まかいの牧場': '', '業務スーパー': '', 'EXCELSIOR CAFFE': '', 'KAMUKURA': '', 'LAWSON': 'お客様控え', 'EXCELSIOR CAFFEEXCELSIOR CAFFE': '', '山安': '', '文左亭': '', 'KALDI': '', '世界一のアップルパイ': '', 'サービスエリア': '', 'NewDays': '', 'STARBUCKS': '', 'アトレ': '', 'クリエイト': '', ' くら寿司': '', 'お菓子のゆりかご': '', 'ダイソー': '', 'くら寿司': ''}
+import re
+storeDB = ['ダイエー', 'ローゼン', 'イオン', 'FamilyMart', 'セブン-イレブン', '吉野家', 'まかいの牧場', 'EXCELSIOR CAFFE', '業務スーパー', 'KAMUKURA', 'LAWSON', 'EXCELSIOR CAFFEEXCELSIOR CAFFE', '業務スーパー', '山安', '文左亭', 'LAWSON', 'KALDI', '世界一のアップルパイ', 'サービスエリア', 'NewDays', 'STARBUCKS', 'アトレ', 'クリエイト', ' くら寿司', 'お菓子のゆりかご', 'ダイソー', 'くら寿司', ' ロイヤル', 'マック', ' EXCELSIOR CAFFE', '博多劇場', 'オーエスドラッグ', 'サンドラック', 'すき家']
+target = {'ダイエー': '', 'ローゼン': '', 'イオン': '', 'FamilyMart': '領収証', 'セブン-イレブン': '領収書', '吉野家': '', 'まかいの牧場': '', '業務スーパー': '', 'EXCELSIOR CAFFE': '', 'KAMUKURA': '', 'LAWSON': 'お客様控え', 'EXCELSIOR CAFFEEXCELSIOR CAFFE': '', '山安': '', '文左亭': '', 'KALDI': '', '世界一のアップルパイ': '', 'サービスエリア': '', 'NewDays': '', 'STARBUCKS': '', 'アトレ': '', 'クリエイト': '', ' くら寿司': '', 'お菓子のゆりかご': '', 'ダイソー': '', 'くら寿司': '', 'サンドラック': '', 'すき家': ''}
 
 def setting(store_name,text):
   if store_name == "":
@@ -8,8 +9,11 @@ def setting(store_name,text):
     return text
   elif store_name == "ローゼン":
     if len(text) >0:
-      print(text)
-      extra_char = re.match("\*?{[0-9]|[A-Z]}+",text)
+      # print(text)
+      extra_char = re.match("\*?([A-Z]|[a-z]|[0-9])+",text)
+      if not(extra_char):
+        print("NO char")
+        return text
       text = text[extra_char.end():]
       idx = text.find("¥")
       if idx != -1:
