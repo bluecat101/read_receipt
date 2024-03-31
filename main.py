9a5d3c69ec4f62e40dffd72b12da7410814a79f0
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-04-01 02:09:30 +0900

    [Refactoring]

--- main.py
+++ main.py
@@ -5,53 +5,87 @@ import gui as ui
 import record_receipt as recordre
 from tkinter import filedialog
 
-exec_type = "production" # production,develop_reciept,develop_noreceipt
+exec_type = "production" # select type of execution： production,develop_receipt,develop_noreceipt
 
-def main(filename):
-  if exec_type == "develop_noreciept":
-    Itemline=[
-    ['つぶグミP濃厚ぶどう', '緑茶', '飲み物', 498, '2'],
-    ['かつ丼･小', 'bb', 'aa', '200', 1],
-    # ['揚げせん', 'item', 'cc', 90, '2'],
-    # ['深蒸し緑茶', '緑茶', 'dd', 498, '2'],
-    # ['クーポン嗜好品20%', 'ee', 'aa', '200', 1],
-    # ['揚げせん', 'item', 'ff', 90, '2']
-    ]
+"""
+# TODO
+  - 内税か外税かを計算する
+  - グラフを２ヶ月分横に表示したりする
+  - グラフに対して目的、ジャンルを追加する。
+"""
+def main(image_name):
+  """ 
+  ## Discription
+    Do reading receipt, comfirm got data, save it 
+  
+  ## Args:
+    `image_name (str)`: Name of receipt image to use
+  """
+
+  if exec_type == "develop_noreceipt":
+    # This exec_type is using test data and don't use receipt
+    # Set test data
+    TEST_DATA=[{'item': 'つぶグミP濃厚ぶどう', 'registed_name': '緑茶', 'genre': '飲み物', 'price': 498, 'amount': 2, 'discount': 0},
+              {'item': 'ブラックサンダー¥', 'registed_name': 'ブラックサンダー', 'genre': 'お菓子', 'price': 38, 'amount': 1, 'discount': 0}
+              ]
+    
+    item_line = TEST_DATA
     date="2023/4/9"
     store="ダイエー"
-    # specialDiscount =[["フルグラ",50],["割引",532]]
-    specialDiscount = []
-  elif exec_type == "develop_reciept":
-    receiptInfo=readre.ReadReceipt(filename,exec_type)
-    Itemline=receiptInfo.getItemLine()
-    store=receiptInfo.getStore()
-    date=receiptInfo.getDate()
-    specialDiscount = receiptInfo.specialDiscount
+    special_discount = []
+  elif exec_type == "develop_receipt":
+    # this exec_type is use receipt data but don't read receipt and
+    # set receipt data before exec this code.
+
+    # Make ReadRecipt instance and read receipt
+    receipt_info=readre.ReadReceipt(image_name,exec_type)
+    # Get product details  line by line 
+    item_line=receipt_info.getItemLine()
+    # Get store name from receipt
+    store=receipt_info.getStore()
+    # Get Date from receipt
+    date=receipt_info.getDate()
+    # Get special discount data from receipt because
+    # special discout does not belong to any product
+    special_discount = receipt_info.special_discount
   elif exec_type == "production":
-    receiptInfo=readre.ReadReceipt(filename)
-    Itemline=receiptInfo.getItemLine()
-    store=receiptInfo.getStore()
-    date=receiptInfo.getDate()
-    specialDiscount = receiptInfo.specialDiscount
-  # print(Itemline)
-  gui=ui.ComfirmReciept(Itemline,date,store,specialDiscount)
+    # this exec_type is for production.
+    # read receipt image
+
+    # Make ReadRecipt instance and read receipt
+    receipt_info=readre.ReadReceipt(image_name)
+    # Get product details  line by line 
+    item_line=receipt_info.getItemLine()
+    # Get store name from receipt
+    store=receipt_info.getStore()
+    # Get Date from receipt
+    date=receipt_info.getDate()
+    # Get special discount data from receipt because
+    # special discout does not belong to any product
+    special_discount = receipt_info.special_discount
+  # Make Gui object and display got data to comfirm whether tha data is correct
+  gui=ui.ComfirmReceipt(item_line,date,store,special_discount)
+  # to diplay gui until user click "決定(dicision)"
   gui.mainloop()
   
-  # if gui.isOk:
-  recordOb=recordre.RecordReceipt(gui.getAllItem(),gui.newCategory)
+  # Save receipt data to csv file
+  record_db=recordre.RecordReceipt(gui.getAllItem(),gui.newCategory)
   
 
 
 
-
+# Execute first when user execute "python main.py" to read and record receipt
 if __name__ == "__main__":
   if "develop" in exec_type:
+    # For test, call main method with file name develop
     main("develop")
   else:
-    filename = filedialog.askopenfilename(
+    # Get file name that read receipt image file.
+    image_name = filedialog.askopenfilename(
       title = "レシートを選択してください。",
       filetypes = [("Image file", ".png .jpg "),("PNG", ".png"), ("JPEG", ".jpg")], # ファイルフィルタ
       initialdir = "./" # 自分自身のディレクトリ
     )
-    if filename != "":
-      main(filename)
+    # Call main method to read receipt
+    if image_name != "":
+      main(image_name)
