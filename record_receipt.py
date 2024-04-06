9096db5d73af370ece2637cfaa74ec23986db4d0
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-04-06 21:29:23 +0900

    [Refactoring]

--- record_receipt.py
+++ record_receipt.py
@@ -9,79 +9,101 @@
 
 import item_db as iDB
 import store_db as sDB
-import csv
-# import os
-CSV_PATH_NAME = "output.csv"      # save receipt infomation 
-ITEMDB_PATH_NAME = "item_db.py"   # reference item infomation and write for new it
-STOREDB_PATH_NAME = "store_db.py" # reference store name infomation and write for new it
+import csv # To save csv file
+
+CSV_PATH_NAME     = "output.csv"  # To save receipt infomation
+ITEMDB_PATH_NAME  = "item_db.py"  # To reference item infomation and write for new it
+STOREDB_PATH_NAME = "store_db.py" # To reference store name infomation and write for new it
 
 class RecordReceipt:
-  item_DB = iDB.item_DB         # read item from DataBase
-  category_DB = iDB.category_DB # read category name from DataBase
-  store_DB=sDB.store_DB         # read store name from DataBase
+  """ 
+  ## Description
+    This class is to save data such as item on receipts.
+    If there are new item, store name and category name, save them too
 
-  def __init__(self,allItem,newCategoryEn):
-    self.allItem = allItem              # to instance argument
-    self.newCategoryEn = newCategoryEn  # to instance argument
-    # self.date = date                    # to instance argument
-    self.store = allItem[0]["store"]                  # to instance argument
-    self.writeFile()                    # record for CSV file
-    self.addDB()                        # update DataBase
+  ## Attributes:
+    item_DB()         : Item already registered such as milk, potato, beef etc...
+    category_DB()     : Category name already registered such as dairy products, vegetable, meat etc...
+    store_DB()        : Store name already registered
+    all_item()        : Got Data from receipt such as item, price, amount, discount etc...
+    new_category_en() : New category name in English not item name
+    store(str)        : Purchace store name
+  """
+  item_DB     = iDB.item_DB     # Read item
+  category_DB = iDB.category_DB # Read category name
+  store_DB    = sDB.store_DB    # Read store name
 
-  def writeFile(self):
-    """ record receipt for csv file """
-    f = open(CSV_PATH_NAME, mode='a') 
-    writer = csv.writer(f)                  # to write with csv format
-    # writer.writerow([self.date,self.store]) # write date and store name
-    for lineItem in self.allItem:
-      writer.writerow(lineItem.values()) # write item
+  def __init__(self,all_item,new_category_en):
+    """
+    ## Description:
+      This method is constructor and save data.
+    #" Args:
+      all_item (str[][]): Got Data from receipt such as item, price, amount, discount etc...
+      new_category_en (): New category name in English not item name
+    """
+    self.all_item = all_item                # Instance argument
+    self.new_category_en = new_category_en  # Instance argument
+    self.store = all_item[0]["store"]       # Instance argument
+    self.write_file() # Record for CSV file
+    self.add_DB()     # Update DataBase
+
+  def write_file(self):
+    """
+    ## Description:
+      To write item data for CSV file
+    """
+    f = open(CSV_PATH_NAME, mode='a') # Open file
+    writer = csv.writer(f)
+    for line_item in self.all_item:
+      writer.writerow(line_item.values()) # Write item
     f.close()
-    print("write for csv file")
+    print("write for csv file") # Notify me that it was saved successfully
 
-  def addDB(self):
-    """ update DataBase(item_db, store_db) """
-    for lineItem in self.allItem:
-      if not(lineItem["registered_name"] in lineItem["item"]): # whether lineItem is special_name
-        # print(lineItem)
-        iDB.special_name[lineItem["item"]] = [lineItem["registered_name"],str(lineItem["category"])]
-      if not(lineItem["category"] in self.item_DB): # whether it is new category (no infomation for DataBase)
-        self.item_DB[lineItem["category"]] = [] # add new key for item_DB
-        self.item_DB[lineItem["category"]].append(lineItem["registered_name"]) # add item for new key as value
-        self.category_DB[lineItem["category"]] = self.newCategoryEn[lineItem["category"]] # add new key and English value name for category
-      elif not(lineItem["registered_name"] in self.item_DB[lineItem["category"]]): # not new category but it is new item
-        self.item_DB[lineItem["category"]].append(lineItem["registered_name"]) # add item for new key as value
+  def add_DB(self):
+    """
+    ## Description:
+      Add new item, category name and store name
+    """
+    for line_item in self.all_item:
+      if not(line_item["registered_name"] in line_item["item"]): # whether line_item is special_name
+        iDB.special_name[line_item["item"]] = [line_item["registered_name"],str(line_item["category"])]
+      if not(line_item["category"] in self.item_DB): # Whether it is new category (= No infomation for DataBase)
+        # Add new key for item_DB
+        # Make list to include items related to that category
+        # value(type) of item_DB is not str but list so set list.
+        self.item_DB[line_item["category"]] = []                                              
+        self.item_DB[line_item["category"]].append(line_item["registered_name"])              # Add item for new key as value
+        self.category_DB[line_item["category"]] = self.new_category_en[line_item["category"]] # Add new key and English value name for category
+      elif not(line_item["registered_name"] in self.item_DB[line_item["category"]]):          # Not new category but it is new item
+        self.item_DB[line_item["category"]].append(line_item["registered_name"])              # Add item for new key as value
     
-    replaceContent= "" # make sentence to write
+    replace_content= "" # Make sentence to overwrite for file
 
-    for key,val in self.item_DB.items(): # write category name and content of it
-      replaceContent += self.category_DB[key] + " = " +str(val)+'\n'
-    replaceContent += "special_name = " +str(iDB.special_name)+'\n'
+    for key,val in self.item_DB.items(): # Write category name and content of it
+      replace_content += self.category_DB[key] + " = " +str(val)+'\n'
+    replace_content += "special_name = " +str(iDB.special_name)+'\n'
 
     ### make item_DB and category_DB argument ###
-    replaceContent+= "item_DB = {"
+    replace_content+= "item_DB = {"
     for key in self.category_DB:
-      replaceContent += "'" + key + "': " + self.category_DB[key] +", "
-    replaceContent=replaceContent[:-2] # delete space and comma
-    replaceContent += '}\n'
-    replaceContent += "category_DB = " + str(self.category_DB) + '\n'
+      replace_content += "'" + key + "': " + self.category_DB[key] +", "
+    replace_content=replace_content[:-2] # delete space and comma
+    replace_content += '}\n'
+    replace_content += "category_DB = " + str(self.category_DB) + '\n'
 
-    with open(ITEMDB_PATH_NAME,"w") as f: # write for item_DB.py
-      f.write(replaceContent)
+    # Overwrite for item_DB.py
+    with open(ITEMDB_PATH_NAME,"w") as f:
+      f.write(replace_content)
     
     if not(self.store in self.store_DB): # Whether new store name or not
       self.store_DB.append(self.store)
       sDB.keyword[self.store] = ""
+      # Get contents in store_db.py as sentence
       with open(STOREDB_PATH_NAME) as file:
         contents = file.readlines()
+      # Update store name and keyword
       contents[1] = "store_DB = " + str(self.store_DB) + '\n'
       contents[2] = "keyword = " + str(sDB.keyword) + '\n'
+      # Overwrite
       with open(STOREDB_PATH_NAME, mode="w") as file:
-        # print(type(contents))
-        # print(contents)
         file.writelines("".join(contents))
-
-
-
-      # with open(STOREDB_PATH_NAME,"w") as f:  # write for item_DB.py
-      #   f.write("store_DB = " + str(self.store_DB))
-
