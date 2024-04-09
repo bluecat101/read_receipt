e3cbc5e11f46491efbeae927126f6092ebcdecaa
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-04-10 02:11:25 +0900

    [Add] Two types of settings were made: custom period and standard period.

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import plotly.figure_factory as ff
 import pandas as pd
-import datetime as dt
+from datetime import datetime as dt,date as d
 import calendar as cal
 from dateutil.relativedelta import relativedelta
 import dash_defer_js_import as dji
@@ -15,7 +15,7 @@ import random
 
 period  = ["今月", "先月", "今年", "昨年", "累計"]
 purpose = ["家族", "父", "全て"]
-
+selected_period = period[0]
 # 付け加え　色
 # colors = {
 #   'background': 'lightblue  ',
@@ -30,15 +30,15 @@ df["日"] = df["日付"].str.split("-").apply(lambda row: int(row[2]))
 
 dashboard = dash.Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP,'./assets/stylesheet.css'])
 def get_data_for_period(data = None,period = None,):
-  today = dt.datetime.today()
+  today = d.today()
   if data is None:
     data = df
   if period is None:
      period = "今月"
   if period == "今月":
     return data[(data["年"] == today.year) & (data["月"] == today.month)]
-    # start_day = dt.datetime(today.year,today.month,1)
-    # end_day = dt.datetime(today.year,today.month,1)+relativedelta(months=1)
+    # start_day = dt(today.year,today.month,1)
+    # end_day = dt(today.year,today.month,1)+relativedelta(months=1)
   elif period == "先月":
     if today.month == 1:
       return data[(data["年"] == today.year-1) & (data["月"] == 12)] #1月 => 12月
@@ -55,7 +55,7 @@ def get_data_for_period(data = None,period = None,):
   return data[(start_day <= pd.to_datetime(data["日付"])) & (pd.to_datetime(data["日付"]) < end_day)]
 
 def get_data_for_purpose(data = None,purpose = None,):
-  today = dt.datetime.today()
+  today = d.today()
   if data is None:
     data = df
   if purpose is None:
@@ -110,30 +110,32 @@ dashboard.layout = dbc.Container([
   dbc.Row([
     dbc.Col([
       dbc.Row([
-        dbc.Col(
-        width = 6,id = "expense"),
         dbc.Col([
-          # デフォルト値を今月にしてドロップダウンを作成
+          dbc.Row(id = "expense"),
           dcc.Dropdown(
-            id = 'dropdown_for_left_side_period',
-            options = period_options,
-            value = period_options[0]["value"]
-          ),
-          dcc.Dropdown(
-            id = 'dropdown_for_left_side_purpose',
+            id = 'dropdown_purpose',
             options = purpose_options,
-            value = purpose_options[0]["value"]
-          ),
-        ],width = 6),
+            value = purpose_options[0]["value"],
+            style={"width": "100px"}
+          ),],
+        width = 6),
+        dbc.Col([dcc.RadioItems(options=period_options,value=period_options[0]["value"],inline=True, id = "radio_period_standard"),
+          dcc.DatePickerRange(id='calender_period_custom',
+             max_date_allowed = d.today(),
+             start_date = d(d.today().year,d.today().month,1),
+             end_date= d.today()
+          )
+         
       ]),
-      dbc.Row(id="genre_chart_at_left_side"),
+      ]),
+      dbc.Row(id="genre_pie_chart_and_ranking_table"),
       dbc.Row([
         html.Label([
           dcc.Input(
           id="input_year",
           placeholder="2024",
           type="number",
-          value= dt.datetime.today().year-1,
+          value= d.today().year,
           style={"width": "100px"}
           ),
           html.Span("年"),
@@ -144,19 +146,16 @@ dashboard.layout = dbc.Container([
         # figure = fig_bar
       ),
     ],width=6),
-    dbc.Col([
+    dbc.Col([ ## 右側
       html.Div(
-        html.P("選択可能(日時、)",style={"height":"100%",
-                      "background-color":"lightblue"}),
+        
       ),
       dcc.Graph(
         id = "right-bar-grath",
         figure = fig_right_bar
       ),
       dbc.Row([
-        dbc.Col(dcc.Graph(id = "right-pie-graph", 
-                      figure=fig_right_pie),width=6),
-        dbc.Col(html.Table([
+        html.Table([
             html.Thead([
               html.Tr([html.Th('順位'), html.Th('名前'), html.Th('スコア')])
             ]),
@@ -168,13 +167,8 @@ dashboard.layout = dbc.Container([
               ], className='table-primary' if rank % 2 == 0 else 'table-secondary')
               for rank, (name, score) in df_ranked.iterrows()
             ])
-          ], className='table table-hover'),width=6)
-
-        
-        
+          ], className='table table-hover')
       ])
-      
-      
     ],
     width=6)
   ])
@@ -235,49 +229,92 @@ def genre_ranking_table_at_left_side(data):
   #       sort_action = 'native',
   # )
   return table
+def get_period_from_word(period):
+  today = d.today()
+  this_year  = today.year
+  this_month = today.month
+  last_year  = this_year - 1
+  last_month = (today - relativedelta(months=1)).month
+  if period == "今月":
+    start_date = "{0}-{1}-1".format(this_year,this_month)
+    end_date = "{0}-{1}-{2}".format(this_year,this_month,cal.monthrange(this_year,this_month)[1])
+  elif period == "先月":
+    start_date = "{0}-{1}-1".format(this_year,last_month)
+    end_date = "{0}-{1}-{2}".format(this_year,last_month,cal.monthrange(this_year,last_month)[1])
+  elif period == "今年":
+    start_date = "{0}-1-1".format(this_year)
+    end_date = "{0}-12-31".format(this_year)
+  elif period == "昨年":
+    start_date = "{0}-1-1".format(last_year)
+    end_date = "{0}-12-31".format(last_year)
+  elif period == "累計":
+    start_date = df.sort_values("日付").iloc[0]["日付"] # df.sort_values("日付")["日付"]だとindex番号を参照してしまいsort後の結果を得られない(sortによってindex番号も一緒にずれており、index番号を取得するともとの部分を取得していることと変わらないため)
+    end_date = str(today)
+  return [start_date,end_date]
+
+def get_triggered_element_for_period(*args):
+  if dash.callback_context.triggered:
+    trigger_id = dash.callback_context.triggered[0]['prop_id'].split('.')[0] # "triggered_name.value"という形で格納されているのでtriggeredの部分を取得
+    print(dash.callback_context.triggered_id)
+    if trigger_id == "radio_period_standard":
+      # return get_period_from_word(args[0])
+      return args[0]
+  #   elif trigger_id == "calender_period_custom":
+  #     return [args[1],args[2]]
+  return selected_period # default値
 
 @dashboard.callback(
   dash.dependencies.Output("expense","children"),
-  dash.dependencies.Input("dropdown_for_left_side_period","value"),
-  dash.dependencies.Input("dropdown_for_left_side_purpose","value")
+  dash.dependencies.Output("genre_pie_chart_and_ranking_table","children"),
+  dash.dependencies.Output("calender_period_custom","start_date"),
+  dash.dependencies.Output("calender_period_custom","end_date"),
+  dash.dependencies.Input("radio_period_standard","value"),
+  dash.dependencies.Input("calender_period_custom","start_date"),
+  dash.dependencies.Input("calender_period_custom","end_date"),
+  dash.dependencies.Input("dropdown_purpose","value"),
 )
-def update_expense(period,purpose):
+def update_genre_chart_at_left_side(radio_period, start_date, end_date, purpose):
+  if dash.callback_context.triggered and dash.callback_context.triggered_id == "radio_period_standard":
+    period = get_period_from_word(radio_period)
+  else:
+    period = [start_date,end_date]
+  # global selected_period ## 目的を更新したときにperiodを保管しておかないとどちらが最後に選択されているのかがわからないため
+  # selected_period = get_triggered_element_for_period(radio_period, start_date, end_date)
+
+  # print(purpose,selected_period)
   total = get_data_for_period(data = get_data_for_purpose(purpose = purpose),period = period)["金額"].sum()
-  return  html.Div(
+  expense_html = html.Div(
             html.P("支出: "+str(total)),
             # style={"hight"}
             # className="px-auto"
           )
-
-@dashboard.callback(
-  dash.dependencies.Output("genre_chart_at_left_side","children"),
-  dash.dependencies.Input("dropdown_for_left_side_period","value"),
-  dash.dependencies.Input("dropdown_for_left_side_purpose","value")
-)
-def update_genre_chart_at_left_side(period,purpose):
+  
   data = get_data_for_period(data = get_data_for_purpose(purpose = purpose),period = period).groupby("ジャンル")["金額"].sum()
   if data.empty:
-    return html.Div(html.P("データがありません",className="border border-4 border-dark",style={"margin-top":"100px"}),style={"height":"250px"})
+    return expense_html, html.Div(html.P("データがありません",className="border border-4 border-dark",style={"margin-top":"100px"}),style={"height":"250px"}), period[0], period[1]
   fig_pie = genre_pie_chart_at_left_side(data)
   table = genre_ranking_table_at_left_side(data)
-  return  dbc.Row([
+  graph_html = dbc.Row([
             dbc.Col([
               # genre_pie_chart_at_left_side(period)
               html.Div(dcc.Graph(figure=fig_pie))
             ],width = 4),
             dbc.Col([
               table
-            ],width = 5,id="genre_ranking_table_at_left_side",className="offset-md-3"),
+            ],width = 5,id="genre_ranking_table",className="offset-md-3"),
           ])
+  return expense_html, graph_html, period[0], period[1]
 
 @dashboard.callback(
   dash.dependencies.Output("year_bar","figure"),
   dash.dependencies.Input("input_year","value"),
+  dash.dependencies.Input("dropdown_purpose","value"),
 )
-def update_year_bar_chart_at_left_side(year):
+def update_year_bar_chart_at_left_side(year,purpose):
   if year is None: # 未入力時
     return dash.no_update
-  data = df[df["年"] == year].groupby(["月","ジャンル"])[["金額"]].sum()
+  data = get_data_for_purpose(purpose = purpose)
+  data = data[data["年"] == year].groupby(["月","ジャンル"])[["金額"]].sum()
   # if year == 2023: print(data)
   is_empty = False
   if data.empty:
@@ -315,3 +352,20 @@ def update_year_bar_chart_at_left_side(year):
 
 if __name__=='__main__':
     dashboard.run_server(debug=True)
+
+
+    #   global selected_period
+#   selected_period = get_triggered_element_for_period(radio_period, start_date, end_date)
+
+#   # if period is None:
+#   #   return dash.no_update
+
+  
+
+# @dashboard.callback(
+#   dash.dependencies.Output("genre_pie_chart_and_ranking_table","children"),
+#   dash.dependencies.Input("radio_period_standard","value"),
+#   dash.dependencies.Input("calender_period_custom","start_date"),
+#   dash.dependencies.Input("calender_period_custom","end_date"),
+#   dash.dependencies.Input("dropdown_purpose","value"),
+# )
