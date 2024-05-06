0e71971693dd224d33e9b855a0cedb020142bd61
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-05-07 01:05:02 +0900

    Create part to select year and month

--- dashboard/index.py
+++ dashboard/index.py
@@ -13,13 +13,30 @@ import calendar as cal
 from dateutil.relativedelta import relativedelta
 import dash_defer_js_import as dji
 import random
+import regex as re
 
 import common as co
 page = "index_"
 
+
+def get_month(df):
+  date = pd.DataFrame(df["日付"].map(lambda x: x.split("-")[0] + "-" + x.split("-")[1]).unique(),columns=["年月"]).sort_values("年月")["年月"].tolist()
+  pre_year = ""
+  for i,x in enumerate(date):
+    year = x.split("-")[0]
+    month = re.sub("^0","",x.split("-")[1])
+    if year == pre_year:
+      date[i] = "　　　" + month + "月"
+    else:
+      date[i] = year + "年" + month + "月"
+      pre_year = year
+  return date
+
 dash.register_page(__name__, path='/')
 df = pd.read_csv('output.csv')
-period  = ["今月", "先月", "今年", "昨年", "累計"]
+co.devide_date(df)
+
+period  = ["今月", "先月", "累計"]
 
 period_options =[{"label": x,"value": x} for x in period]
 
@@ -35,10 +52,9 @@ dbc.Row([
     dbc.Row([
       co.create_col_button_expense_purpose(page),
       dbc.Col([dcc.RadioItems(options=period_options,value=period_options[0]["value"],inline=True, id = f"{page}radio_period_standard"),
-        dcc.Dropdown(options = [{"label": x, "value": x} for x in ["1月","2月"]])  
+        dcc.Dropdown(options = [{"label": x, "value": x} for x in get_month(df)])  
       ]),
     ]),
   ],width=6),
   dbc.Col(width=6)
 ]),width=9, id = "main")])
-
